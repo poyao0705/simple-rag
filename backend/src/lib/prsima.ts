48ef999a2f016609651b0cf01209632fbd871bd1
poyaohuang <poyaohg0705@gmail.com>
2026-07-12 20:58:18 +1000

    feat: add prisma client singleton

---
+++ backend/src/lib/prsima.ts
@@ -0,0 +1,12 @@
+// src/lib/prisma.ts
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const connectingString = process.env.DATABASE_URL;
+if (!connectingString) {
+	throw new Error("DATABASE_URL is not set");
+}
+
+export const prisma = new PrismaClient({
+	adapter: new PrismaPg(connectingString),
+});
